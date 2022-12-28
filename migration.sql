1f49a3b85f1b35f947a2d49a07ffcc5550954996
ezra <51865478+onion-boy@users.noreply.github.com>
2022-12-27 21:28:07 -0500

    changing triggers

--- migration.sql
+++ migration.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 14.4
 -- Dumped by pg_dump version 14.4
 
--- Started on 2022-12-27 18:43:39
+-- Started on 2022-12-27 21:27:52
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -19,11 +19,11 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 3325 (class 1262 OID 32833)
+-- TOC entry 3327 (class 1262 OID 32833)
 -- Name: openDB; Type: DATABASE; Schema: -; Owner: openDB
 --
 
-CREATE DATABASE "openDB" WITH TEMPLATE = template0 ENCODING = 'UTF8';
+CREATE DATABASE "openDB" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'English_United States.1252';
 
 
 ALTER DATABASE "openDB" OWNER TO "openDB";
@@ -46,33 +46,33 @@ SET row_security = off;
 -- Name: users; Type: SCHEMA; Schema: -; Owner: openDB
 --
 
-CREATE SCHEMA "users";
+CREATE SCHEMA users;
 
 
-ALTER SCHEMA "users" OWNER TO "openDB";
+ALTER SCHEMA users OWNER TO "openDB";
 
 --
 -- TOC entry 832 (class 1247 OID 32852)
 -- Name: composite_user; Type: TYPE; Schema: users; Owner: openDB
 --
 
-CREATE TYPE "users"."composite_user" AS (
-	"user_id" character varying(15),
-	"email" character varying(256),
-	"username" character varying(256),
-	"created" "date"
+CREATE TYPE users.composite_user AS (
+	unique_id character varying(15),
+	email character varying(256),
+	username character varying(256),
+	created date
 );
 
 
-ALTER TYPE "users"."composite_user" OWNER TO "openDB";
+ALTER TYPE users.composite_user OWNER TO "openDB";
 
 --
 -- TOC entry 215 (class 1255 OID 32864)
 -- Name: user_created_trigge(); Type: FUNCTION; Schema: public; Owner: postgres
 --
 
-CREATE FUNCTION "public"."user_created_trigge"() RETURNS "trigger"
-    LANGUAGE "plpgsql"
+CREATE FUNCTION public.user_created_trigge() RETURNS trigger
+    LANGUAGE plpgsql
     AS $$
 BEGIN
     NEW.user_id = users.alphanumeric();
@@ -81,15 +81,15 @@ END;
 $$;
 
 
-ALTER FUNCTION "public"."user_created_trigge"() OWNER TO "postgres";
+ALTER FUNCTION public.user_created_trigge() OWNER TO postgres;
 
 --
 -- TOC entry 214 (class 1255 OID 32861)
 -- Name: alphanumeric(integer); Type: FUNCTION; Schema: users; Owner: openDB
 --
 
-CREATE FUNCTION "users"."alphanumeric"("size" integer DEFAULT 15) RETURNS "text"
-    LANGUAGE "plpgsql"
+CREATE FUNCTION users.alphanumeric(size integer DEFAULT 15) RETURNS text
+    LANGUAGE plpgsql
     AS $$DECLARE
     str varchar(25);
 BEGIN
@@ -98,69 +98,69 @@ BEGIN
 END;$$;
 
 
-ALTER FUNCTION "users"."alphanumeric"("size" integer) OWNER TO "openDB";
+ALTER FUNCTION users.alphanumeric(size integer) OWNER TO "openDB";
 
 --
--- TOC entry 216 (class 1255 OID 32865)
--- Name: assign_user_id(); Type: FUNCTION; Schema: users; Owner: postgres
+-- TOC entry 219 (class 1255 OID 32865)
+-- Name: assign_unique_id(); Type: FUNCTION; Schema: users; Owner: openDB
 --
 
-CREATE FUNCTION "users"."assign_user_id"() RETURNS "trigger"
-    LANGUAGE "plpgsql"
+CREATE FUNCTION users.assign_unique_id() RETURNS trigger
+    LANGUAGE plpgsql
     AS $$
 BEGIN
-    NEW.user_id = users.alphanumeric();
+    NEW.unique_id = users.alphanumeric();
     RETURN NEW;
 END;
 $$;
 
 
-ALTER FUNCTION "users"."assign_user_id"() OWNER TO "postgres";
+ALTER FUNCTION users.assign_unique_id() OWNER TO "openDB";
 
 SET default_tablespace = '';
 
-SET default_table_access_method = "heap";
+SET default_table_access_method = heap;
 
 --
 -- TOC entry 213 (class 1259 OID 32853)
 -- Name: composite_user; Type: TABLE; Schema: public; Owner: openDB
 --
 
-CREATE TABLE "public"."composite_user" (
-    "user_id" integer,
-    "email" character varying(256),
-    "username" character varying(15),
-    "created" "date"
+CREATE TABLE public.composite_user (
+    user_id integer,
+    email character varying(256),
+    username character varying(15),
+    created date
 );
 
 
-ALTER TABLE "public"."composite_user" OWNER TO "openDB";
+ALTER TABLE public.composite_user OWNER TO "openDB";
 
 --
 -- TOC entry 210 (class 1259 OID 32835)
 -- Name: basic; Type: TABLE; Schema: users; Owner: openDB
 --
 
-CREATE TABLE "users"."basic" (
-    "id" integer NOT NULL,
-    "full_name" character varying(256),
-    "email" character varying(256) NOT NULL,
-    "username" character varying(15) NOT NULL,
-    "created" "date" NOT NULL,
-    "deleted" "date",
-    "user_id" character varying(15) NOT NULL
+CREATE TABLE users.basic (
+    id integer NOT NULL,
+    full_name character varying(256),
+    email character varying(256) NOT NULL,
+    username character varying(15) NOT NULL,
+    created date NOT NULL,
+    deleted date,
+    unique_id character varying(15) NOT NULL
 );
 
 
-ALTER TABLE "users"."basic" OWNER TO "openDB";
+ALTER TABLE users.basic OWNER TO "openDB";
 
 --
 -- TOC entry 211 (class 1259 OID 32840)
 -- Name: basic_user_id_seq; Type: SEQUENCE; Schema: users; Owner: openDB
 --
 
-ALTER TABLE "users"."basic" ALTER COLUMN "id" ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME "users"."basic_user_id_seq"
+ALTER TABLE users.basic ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME users.basic_user_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -174,30 +174,39 @@ ALTER TABLE "users"."basic" ALTER COLUMN "id" ADD GENERATED ALWAYS AS IDENTITY (
 -- Name: basic basic_pkey; Type: CONSTRAINT; Schema: users; Owner: openDB
 --
 
-ALTER TABLE ONLY "users"."basic"
-    ADD CONSTRAINT "basic_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY users.basic
+    ADD CONSTRAINT basic_pkey PRIMARY KEY (id);
 
 
 --
--- TOC entry 3179 (class 2606 OID 32846)
--- Name: basic username_email_unique; Type: CONSTRAINT; Schema: users; Owner: openDB
+-- TOC entry 3179 (class 2606 OID 32874)
+-- Name: basic email_username_uniques; Type: CONSTRAINT; Schema: users; Owner: openDB
 --
 
-ALTER TABLE ONLY "users"."basic"
-    ADD CONSTRAINT "username_email_unique" UNIQUE ("username", "email");
+ALTER TABLE ONLY users.basic
+    ADD CONSTRAINT email_username_uniques UNIQUE (email, username);
 
 
 --
--- TOC entry 3180 (class 2620 OID 32866)
+-- TOC entry 3181 (class 2606 OID 32876)
+-- Name: basic id_unique; Type: CONSTRAINT; Schema: users; Owner: openDB
+--
+
+ALTER TABLE ONLY users.basic
+    ADD CONSTRAINT id_unique UNIQUE (unique_id);
+
+
+--
+-- TOC entry 3182 (class 2620 OID 32866)
 -- Name: basic beforeuserupdate; Type: TRIGGER; Schema: users; Owner: openDB
 --
 
-CREATE TRIGGER "beforeuserupdate" BEFORE INSERT ON "users"."basic" FOR EACH ROW EXECUTE FUNCTION "users"."assign_user_id"();
+CREATE TRIGGER beforeuserupdate BEFORE INSERT ON users.basic FOR EACH ROW EXECUTE FUNCTION users.assign_unique_id();
 
 
 --
--- TOC entry 3326 (class 0 OID 0)
--- Dependencies: 3325
+-- TOC entry 3328 (class 0 OID 0)
+-- Dependencies: 3327
 -- Name: DATABASE "openDB"; Type: ACL; Schema: -; Owner: openDB
 --
 
@@ -207,7 +216,7 @@ GRANT CREATE,CONNECT ON DATABASE "openDB" TO "openDB";
 GRANT TEMPORARY ON DATABASE "openDB" TO "openDB" WITH GRANT OPTION;
 
 
--- Completed on 2022-12-27 18:43:39
+-- Completed on 2022-12-27 21:27:53
 
 --
 -- PostgreSQL database dump complete
--- src/routes/user.rs
+++ src/routes/user.rs
@@ -19,13 +19,13 @@ pub struct CreateUserForm {
 
 #[derive(Serialize)]
 pub struct UserId {
-    user_id: String,
+    unique_id: String,
 }
 
 #[derive(Serialize, sqlx::Type)]
 #[sqlx(type_name = "composite_user")]
 pub struct User {
-    user_id: String,
+    unique_id: String,
     email: String,
     username: String,
     created: NaiveDate,
@@ -35,17 +35,17 @@ pub struct User {
 pub struct LookupUserForm {
     username: Option<String>,
     email: Option<String>,
-    user_id: Option<String>,
+    id: Option<String>,
 }
 
 pub async fn create_new_user(
     State(pool): State<PgPool>,
     form: Form<CreateUserForm>,
 ) -> Result<Json<UserId>, (StatusCode, String)> {
-    let created = chrono::Utc::now().date_naive();
+    let created = chrono::Local::now().date_naive();
 
     let new_user = sqlx::query_as!(UserId,
-        "INSERT INTO \"users\".\"basic\" (full_name, email, username, created) VALUES ($1::varchar(256), $2::varchar(256), $3::varchar(15), $4::date) RETURNING \"user_id\"",
+        "INSERT INTO \"users\".\"basic\" (full_name, email, username, created) VALUES ($1::varchar(256), $2::varchar(256), $3::varchar(15), $4::date) RETURNING \"unique_id\"",
         form.full_name, form.email, form.username, created)
         .fetch_one(&pool)
         .await;
@@ -71,16 +71,16 @@ pub async fn lookup_user(
     } else if let Some(email) = query.email.clone() {
         detail = Some("email");
         Some(email)
-    } else if let Some(user_id) = query.user_id.clone() {
-        detail = Some("user_id");
-        Some(user_id)
+    } else if let Some(id) = query.id.clone() {
+        detail = Some("unique_id");
+        Some(id)
     } else {
         detail = None;
         None
     };
 
     if let Some(detail_name) = detail {
-        let query = format!("SELECT (user_id,email,username,created)::users.composite_user FROM \"users\".\"basic\" WHERE \"{}\" = $1", detail_name);
+        let query = format!("SELECT (unique_id,email,username,created)::users.composite_user FROM \"users\".\"basic\" WHERE \"{}\" = $1", detail_name);
         sqlx::query_as(&query)
             .bind(value.unwrap())
             .fetch_one(&pool)
